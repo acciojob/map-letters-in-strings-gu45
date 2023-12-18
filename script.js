c809e8c9a11699b1275cd2a0ba136f8dbcf31aee
gu45 <86247735+gu45@users.noreply.github.com>
2023-12-18 18:19:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+let str = "dododo"
+let ans = {}
+
+for(let i=0;i<str.length;i++){
+  if(ans[str[i]]){
+    ans[str[i]].push(i);
+  }
+  else{
+    ans[str[i]] = [i]
+  }
+}
+console.log(ans)
